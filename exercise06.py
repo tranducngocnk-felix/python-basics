0392643a8e54c8ba64847b703abf38ce782b446e
Trần Ngọc <tranducngocnk@gmail.com>
2026-08-05 10:26:00 +0700

    Complete Python Lesson 06 - Lists

---
+++ exercise06.py
@@ -0,0 +1,19 @@
+subjects = ["Python", "SQL", "Linux", "Git", "Docker"]
+print(subjects)
+print(subjects[0])
+print(subjects[4])
+#======Bài 2=========
+langues = ["Python", "Java", "C++"]
+langues.append("Go")
+langues[1] = "JavaScript"
+langues.remove("C++")
+print(langues)
+#======Bài 3=========
+tasks =[]
+tasks.append("Study Python")
+tasks.append("Study SQL")
+tasks.append("Study Linux")
+print(tasks)
+tasks.remove("Study SQL")
+print(tasks)
+print(len(tasks))
---
+++ lesson06.py
@@ -0,0 +1,6 @@
+numbers = [10, 20, 30, 40]
+print(numbers)
+numbers.remove(20)
+numbers.append(50)
+print(numbers)
+print(len(numbers))
